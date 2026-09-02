afc7e1a6cefc01a7ab298a563149c168acc4a717
SamBooth <shb1713@rit.edu>
2026-09-02 14:23:43 -0400

    refactored

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const http = require('http');
-const fs = require('fs');
+const responses = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
 
 const onRequest = (request, response) => {
     console.log(request.url);
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(index);
-    response.end();
+    responses.getIndex(request, response);
 };
 
 http.createServer(onRequest).listen(port, ()=>{
